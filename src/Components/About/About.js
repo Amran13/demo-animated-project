09797bbc13cc03230600d14503f842c5187c0741
amranhossain <amranhossainchy@gmail.com>
2022-11-03 10:49:41 +0600

    all set. ready to go >> animation

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,12 +5,12 @@ import { Button } from 'react-bootstrap';
 
 const About = () => {
     return (
-        <div className='about-container'>
+        <div className='about-container p-3'>
             <div className='mx-5'>
-            <img src={avatar} alt="" />
+            <img  src={avatar} alt="" />
             </div>
             <div className='mx-5'>
-                <h2 className='text-danger'>This is me</h2>
+                <h2 className='text-danger fw-bold'>About</h2>
                 <p className="text-light my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nulla quasi dicta, qui quis aliquam nobis odit tempore alias deserunt quae aliquid quibusdam reiciendis aperiam facere distinctio. Fugiat fuga, magnam harum omnis dolore fugit est suscipit aperiam, tenetur temporibus minus!</p>
                 <Button className='my-3' variant="danger">RESUME</Button>
             </div>
